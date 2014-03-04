05d7489e166471b6353711c144e79c8a910284d2
bplatta <bmplatta@gmail.com>
2014-03-04 01:30:20 -0600

    rebind new el to delete

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){ 
     var current="";
+    var count = 0;
     var display = $('#outputInfo');
     var favorites = $('#hiddendata span').html().split(',');
-
-    $.fn.refresh = function() {
-    return $(this.selector);
-    };
 
     $('.spotset').click(function(){
         current = $('#current').val();
@@ -28,7 +25,7 @@ $(document).ready(function(){
         }
     });
 
-    $('.deleteFav').click(function(){
+    $('.deleteFav').bind("click", function(){
         var oldFav = $(this).parent().html().split('<')[0];
         favorites.splice(favorites.indexOf(oldFav),1);
 
@@ -38,13 +35,25 @@ $(document).ready(function(){
     });
 
     $('.addFav').click(function() {
+        count += 1;
         var newItem = $('#addFav').val();
         console.log(newItem);
         if(newItem != "") {
             favorites.push(newItem);
-            var addLi = '<li>' + newItem + '<img src="images/delete.png" class="deleteFav">';
+            var addLi = '<li>' + newItem + '<img src="images/delete.png" id="deleteFav-' + count + '"' + '></li>';
             $(this).parent().parent().prepend(addLi);
             console.log(favorites);
+            console.log(addLi);
+
+            var newEl = "#deleteFav-" + count;
+            $(newEl).bind("click", function(){
+                var oldFav = $(this).parent().html().split('<')[0];
+                favorites.splice(favorites.indexOf(oldFav),1);
+
+                $(this).parent().remove();
+                console.log(oldFav + " removed from favorites");
+                console.log(favorites);
+            });
         }
     });
 });
