a5cce7237ba7bb8ae900e947e314c01eb683607b
bplatta <bmplatta@gmail.com>
2014-03-06 20:52:31 -0600

    css update

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
 	$('#mag-click').click(function(){
-		if($(this).parent().hasClass("opened")) {
-			$(this).parent().animate({left:'-19em'}, {queue: false, duration: 500}).removeClass("opened");
+		if($(this).parent().parent().hasClass("opened")) {
+			$(this).parent().parent().animate({left:'-19em'}, {queue: false, duration: 500}).removeClass("opened");
 		} else {
-			$(this).parent().animate({left:'0em'}, {queue: false, duration: 500}).addClass("opened");
+			$(this).parent().parent().animate({left:'0em'}, {queue: false, duration: 500}).addClass("opened");
 		}
 	});
 
