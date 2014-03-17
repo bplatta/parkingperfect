6fc433adf58327c1f933e165c10693f04201f089
bplatta <bmplatta@gmail.com>
2014-03-17 04:47:46 -0500

    style list view

--- data/index.js
+++ data/index.js
@@ -1,40 +1,40 @@
 var data = {"results":[{
-		"where": "North of Sherman and Clark",
+		"where": ["North of Sherman and Clark", "north"],
 		"distance": "6",
 		"permit": "None",
 		"when": "8AM-9PM",
 		"price": "Metered"
 	},
 	{
-		"where": "South of Foster and Maple",
+		"where": ["South of Foster and Maple", "south"],
 		"distance": "1",
 		"permit": "Res-A",
 		"when": "Any",
 		"price": "Free"
 	},
 	{
-		"where": "East of Ridge and Noyes",
+		"where": ["East of Ridge and Noyes", "east"],
 		"distance": "9",
 		"permit": "Res-A",
 		"when": "Any",
 		"price": "Free"
 	},
 	{
-		"where": "North of Sherman and Clark",
+		"where": ["North of Sherman and Clark", "north"],
 		"distance": "3",
 		"permit": "None",
 		"when": "8AM-9PM",
 		"price": "Metered"
 	},
 	{
-		"where": "South of Foster and Maple",
+		"where": ["South of Foster and Maple", "south"],
 		"distance": "4",
 		"permit": "Res-A",
 		"when": "Any",
 		"price": "Free"
 	},
 	{
-		"where": "East of Ridge and Noyes",
+		"where": ["West of Ridge and Noyes", "west"],
 		"distance": "2",
 		"permit": "Res-A",
 		"when": "Any",
